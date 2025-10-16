d368aeca40512f6671b8b7e374f1dd30b8d9d394
jungwook <mr4454@naver.com>
2025-10-16 19:54:38 +0900

    feat(delimiterList): implement delimiterList
    Implement delimiterList at main:
    - add customDelimiter if it occur

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,17 +2,22 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         String input = guideInput();
+        List<String> delimiterList = new ArrayList<>(Arrays.asList(":",","));
 
         if(hasCustomDelimiter(input)) {
             String customDelimiter = extractCustomDelimiter(input);
 //            System.out.println(customDelimiter);
+            delimiterList.add(customDelimiter);
 
             input = refineInput(input);
 //            System.out.println(input);
-
         }
 
 
