5347c182040f8d4b1a6d51c9213a05ee2b1f71a1
jungwook <mr4454@naver.com>
2025-10-19 01:38:06 +0900

    test(calculateTotalSum): add unit test
    Add unit test on calculateTotalSum
    - case of delimiters with custom delimiters

---
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -0,0 +1,19 @@
+package calculator.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorServiceTest {
+
+    @Test
+    void calculateTotalSum() {
+        List<String> delimiters = Arrays.asList(",", ":", "-");
+        String input = "1,2:3-4";
+        long result = CalculatorService.calculateTotalSum(input, delimiters);
+        assertThat(result).isEqualTo(10);
+    }
+}
